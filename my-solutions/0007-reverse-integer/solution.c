c611545325e368ee67bbfd4027a5d5aeeee1eff4
Lekkala Jeyavardhan <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-07-30 19:29:02 +0000

    [LeetCode Sync] Runtime - 3 ms (55.46%), Memory - 9.1 MB (98.67%)

---
+++ my-solutions/0007-reverse-integer/solution.c
@@ -0,0 +1,17 @@
+int reverse(int x){
+    int revnum = 0;
+
+    while (x != 0){
+        int pop = x % 10;
+        x /= 10;
+        if (revnum > INT_MAX / 10 || (revnum == INT_MAX / 10 && pop > 7)) {
+            return 0;
+        }
+        if (revnum < INT_MIN / 10 || (revnum == INT_MIN / 10 && pop < -8)) {
+            return 0;
+        }
+        revnum = (revnum * 10) + pop;
+    }
+
+    return revnum;
+}
