a65af56fde2e13f25cd7aabdea61df67304e88f5
Lekkala Jeyavardhan <68065109+JeyaVardhan-L@users.noreply.github.com>
2026-08-10 06:58:27 +0000

    [LeetCode Sync] Runtime - 41 ms (67.86%), Memory - 14.3 MB (25.60%)

---
+++ my-solutions/2508-maximum-sum-of-an-hourglass/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maxSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        maxSum = float('-inf')
+        for i in range(len(grid)-2):
+            for j in range(len(grid[0])-2):
+                s = grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]
+                maxSum = max(s, maxSum)
+        return maxSum
